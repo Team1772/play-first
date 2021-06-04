f8f5abe06a643ccff39f95f69527fa96db271a9f
Gabriela Tomaz <gabrielatomaz1772@gmail.com>
2021-06-03 23:10:10 -0300

    added lower case to url

--- src/components/GameSelector.vue
+++ src/components/GameSelector.vue
@@ -6,7 +6,7 @@
                     <figure class="image is-square">
                         <img :src="src" :alt="alt">
                     </figure>
-                    <Button :text="button.text" :event="() => goTo(`/${name}`)" />
+                    <Button :text="button.text" :event="() => goTo(`/${name.toLowerCase()}`)" />
                 </div>
             </div>
         </div>
